33e536455ae28cfc88b6faa48394348c57ac8934
deretou <deretou@gmail.com>
2019-11-07 09:40:37 -0600

    Chatroom

---
+++ src/main/java/edu/udacity/java/nano/controller/WebSocketChatServer.java
@@ -0,0 +1,76 @@
+package edu.udacity.java.nano.controller;
+
+
+import edu.udacity.java.nano.model.Message;
+import org.springframework.stereotype.Component;
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import com.alibaba.fastjson.JSON;
+
+/**
+ * WebSocket Server
+ *
+ * @see ServerEndpoint WebSocket Client
+ * @see Session   WebSocket Session
+ */
+
+@Component
+@ServerEndpoint("/chat")
+public class WebSocketChatServer {
+
+    /**
+     * All chat sessions.
+     */
+    private static Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
+
+    private static void sendMessageToAll(String msg) {
+        onlineSessions.forEach((id, session) -> {
+            try {
+                session.getBasicRemote().sendText(msg);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    /**
+     * Open connection, 1) add session, 2) add user.
+     */
+    @OnOpen
+    public void onOpen(Session session) {
+        onlineSessions.put(session.getId(), session);
+        sendMessageToAll(Message.jsonStr("ENTER", "", "", onlineSessions.size()));
+    }
+
+    /**
+     * Send message, 1) get username and session, 2) send message to all.
+     */
+    @OnMessage
+    public void onMessage(Session session, String jsonStr) {
+        Message message = JSON.parseObject(jsonStr, Message.class);
+        sendMessageToAll(Message.jsonStr("CHAT", message.getSender(), message.getContent(), onlineSessions.size()));
+    }
+
+    /**
+     * Close connection, 1) remove session, 2) update user.
+     */
+    @OnClose
+    public void onClose(Session session) {
+        onlineSessions.remove(session.getId());
+        sendMessageToAll(Message.jsonStr("LEAVE", "", "Offline!", onlineSessions.size()));
+    }
+
+    /**
+     * Print exception.
+     */
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+
+}
+
+
---
+++ src/main/java/edu/udacity/java/nano/model/Message.java
@@ -0,0 +1,68 @@
+package edu.udacity.java.nano.model;
+
+
+import com.alibaba.fastjson.JSON;
+/**
+ * WebSocket message model
+ */
+
+public class Message {
+    private MessageType type;
+    private String content;
+    private String sender;
+    private int counter;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int onlineCount) {
+        this.counter = onlineCount;
+    }
+
+    public enum MessageType {
+        ENTER,
+        CHAT,
+        LEAVE
+    }
+
+
+    public Message(MessageType type, String sender, String content, int onlineTotal) {
+        this.type = type;
+        this.sender = sender;
+        this.content = content;
+        this.counter = onlineTotal;
+    }
+
+
+
+    public static String jsonStr(String type, String sender, String content, int onlineTotal) {
+        return JSON.toJSONString(new Message( MessageType.valueOf(type), sender, content, onlineTotal));
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
+
+
